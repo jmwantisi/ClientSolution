0f152e0c7dd49bbd1d368c4597ecd2f18aa071ca
James Mwantisi <jmwantisi@gmail.com>
2024-01-22 14:08:35 +0200

    Setup  routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from './ProtectedRoute';
+import Login from './components/Login';
+import BookingList from './components/Booking/BookingList';
+import 'primeflex/primeflex.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route element={<ProtectedRoute/>} path="bookings" >
+          <Route path="list" element={<BookingList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
